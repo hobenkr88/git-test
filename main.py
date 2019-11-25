8192bd5c5c2a53ef947cb8c0a5f42daf8d8d7026
Kenwood <hobenkr88@gmail.com>
2019-11-25 05:22:04 -0500

    Added formatting

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import time
 
+width = 40
+height = 20
+border_char = "*"
+
 def run():
     t = time.time()
-    print('The time is "+str(t))
+    output = "The time is"+str(t)
+    output_len = len(output)
+    
+    print(border_char*width, end="\n")
+    
+    print(border_char, end="")
+    print(" "*(width-2), end="")
+    print(border_char, end="\n")
+    
+    print(border_char, end="")
+    print(" ", end="")
+    print('The time is '+str(t), end="")
+    print(" "*(width-2-2-1-output_len), end="")
+    print(" ", end="")
+    print(border_char, end="\n")
+    
+    print(border_char, end="")
+    print(" "*(width-2), end="")
+    print(border_char, end="\n")
+    
+    print(border_char*width, end="\n")
 
 
 if __name__ == '__main__':
